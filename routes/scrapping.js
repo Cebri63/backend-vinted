9d7e4855088e55c156ab62380f0b18a0aa9b8287
Xavier Colombel <xavier@lereacteur.io>
2020-08-20 00:03:03 +0200

    fix scraping problem

--- routes/scrapping.js
+++ routes/scrapping.js
@@ -1,16 +1,60 @@
 const puppeteer = require("puppeteer");
+const scrollPageToBottom = require("puppeteer-autoscroll-down");
 
 const URL = "https://www.vinted.fr/";
 
 const getData = async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
   const page = await browser.newPage();
-  await page.goto(URL, { waitUntil: "networkidle2" });
-  await page.waitFor(12000);
+  await page.setViewport({
+    width: 1600,
+    height: 1600,
+  });
+  await page.goto(URL, { waitUntil: "domcontentloaded" });
+  await scrollPageToBottom(page);
+  await page.waitFor(".feed-grid__item");
   const data = await page.evaluate(() => {
-    /* return [...document.querySelectorAll(".feed-grid__item")]; */
-    /* return document.querySelector(".feed-grid").innerHTML; */
-    /* return Array.from(document.querySelectorAll(".feed-grid__item")); */
+    const elements = Array.from(document.querySelectorAll(".feed-grid__item"));
+    return elements.map((element) => {
+      const owner_name = element.querySelector(".c-box__owner .u-flexbox")
+        ? element.querySelector(".c-box__owner .u-flexbox").innerText
+        : null;
+      const owner_image = element.querySelector(".c-box__owner img")
+        ? element.querySelector(".c-box__owner img").getAttribute("src")
+        : null;
+      const owner_link = element.querySelector(".c-box__owner a")
+        ? element.querySelector(".c-box__owner a").getAttribute("href")
+        : null;
+      const product_image = element.querySelector(".c-box__image img")
+        ? element.querySelector(".c-box__image img").getAttribute("src")
+        : null;
+      const product_link = element.querySelector(".c-box__image a")
+        ? `https://www.vinted.fr${element
+            .querySelector(".c-box__image a")
+            .getAttribute("href")}`
+        : null;
+      const product_title = element.querySelector(".c-box__title")
+        ? element.querySelector(".c-box__title").innerText
+        : null;
+      const product_subtitle = element.querySelector(".c-box__subtitle")
+        ? element.querySelector(".c-box__subtitle").innerText
+        : null;
+      const product_details = element.querySelector(".c-box__details")
+        ? element.querySelector(".c-box__details").innerText
+        : null;
+      return {
+        owner_name,
+        owner_image,
+        owner_link,
+        product_image,
+        product_link,
+        product_title,
+        product_subtitle,
+        product_details,
+      };
+    });
   });
 
   console.log(data);
